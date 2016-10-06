b4b9c5ae1a6e915425c02a1e9b8fc2580560b481
vinhcntt90 <vinhcntt900@gmail.com>
2016-10-06 11:06:01 +0700

    Edit lan 2

--- baitaptuan8/bin2dec.cpp
+++ baitaptuan8/bin2dec.cpp
@@ -2,26 +2,16 @@
 
 using namespace std;
 
-int main() {
-	int N; // N là mot so tu nhien o dang nhi phan
-	int b = 1, KQ = 0, i = 0;
-	int L = 0;  // so chu so cua N
-	cin >> N;
-	
-	// tim L
-	int tmp = N;
-	while(N > 0) {
-		N /= 10;
-		L++;
-	}
-	N = tmp;
-	
-	for (i = 0; i < L; i++) {
-		KQ += b * (N % 10);
-		N /= 10;
-		b *= 2;
-	}
-	
-	cout << KQ << endl;
-
-}
+ int main() {
+ 	int N; // N là mot so tu nhien o dang nhi phan
+ 	int b = 1, KQ = 0, i = 0, L;	
+ 	cin >> N;
+ 	while(N != 0) 
+	{
+ 		KQ += b * (N % 10);
+ 		N /= 10;
+ 		b *= 2;
+ 	}
+ 	cout << KQ << endl;
+ 
+ }
