8f9bcd9e3dc5b638bd2adec68a740734a5cd1afc
vinhcntt90 <vinhcntt900@gmail.com>
2016-10-06 22:16:05 +0700

    sua lan 3

--- baitaptuan8/bin2dec.cpp
+++ baitaptuan8/bin2dec.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
  int main() {
  	int N; // N là mot so tu nhien o dang nhi phan
- 	int b = 1, KQ = 0, i = 0, L;	
+ 	int b = 1, KQ = 0, i = 0;	
  	cin >> N;
  	while(N != 0) 
 	{
